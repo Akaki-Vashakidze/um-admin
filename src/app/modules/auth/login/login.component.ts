1a3b5fdc66e54339f635d72f286f10de6eb1c4cf
akakivashakidze@Akakis-MBP.akaki-vashakidze <akakivashakidze@Akakis-MBP.akaki.vashakidze1@gmail.com>
2023-07-12 22:57:12 +0400

    adding login page

--- src/app/modules/auth/login/login.component.ts
+++ src/app/modules/auth/login/login.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  loginForm!: FormGroup;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      token: new FormControl(null, [Validators.required]),
+    });
+  }
+
+  login() {
+    console.log(this.loginForm.value)
+  }
 }
